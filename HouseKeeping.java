2e5cd9d35563f7796391a2ad0679a3f3c7a21aa3
RoyalJonesGo <152839662+RoyalJonesGo@users.noreply.github.com>
2024-10-21 23:47:05 +0800

    Create HouseKeeping.java

---
+++ HouseKeeping.java
@@ -0,0 +1,17 @@
+public class HouseKeeping implements HotelService {
+
+    private int roomNumber;
+
+    public HouseKeeping(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public void cleanRoom() {
+        System.out.println("Cleaning room number: " + roomNumber);
+    }
+
+    @Override
+    public void execute() {
+        cleanRoom();
+    }
+}
