8e50f994eafa06601088f43ea143d1406b9c2fff
RoyalJonesGo <152839662+RoyalJonesGo@users.noreply.github.com>
2024-10-21 23:46:47 +0800

    Create Valet.java

---
+++ Valet.java
@@ -0,0 +1,17 @@
+public class Valet implements HotelService {
+
+    private String plateNumber;
+
+    public Valet(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+
+    public void pickUpVehicle() {
+        System.out.println("Picking up vehicle with plate number: " + plateNumber);
+    }
+
+    @Override
+    public void execute() {
+        pickUpVehicle();
+    }
+}
